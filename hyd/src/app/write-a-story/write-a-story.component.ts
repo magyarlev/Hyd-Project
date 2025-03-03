edda4cab0fe7607fe17897c9d22c29de79d250bf
MagyarL <magyar.levi@gmail.com>
2025-03-03 12:06:27 +0100

    add submit button

--- hyd/src/app/write-a-story/write-a-story.component.ts
+++ hyd/src/app/write-a-story/write-a-story.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-write-a-story',
-  imports: [MatButtonToggleModule, MatInputModule, FormsModule],
+  imports: [
+    MatButtonToggleModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './write-a-story.component.html',
   styleUrl: './write-a-story.component.scss',
 })
