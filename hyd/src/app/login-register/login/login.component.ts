1bc1b69586ef4d9e53625b2d64e06d8c84a637a5
MagyarL <magyar.levi@gmail.com>
2025-03-31 10:30:32 +0200

    add register/login components

---
+++ hyd/src/app/login-register/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, DestroyRef, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../auth.service';
+import { User } from '../../types';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  fb = inject(FormBuilder);
+  loginForm: FormGroup;
+  #authService = inject(AuthService);
+  passwordError?: string;
+  destroyRef = inject(DestroyRef);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.#authService
+        .loginUser(this.loginForm.value as User)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe({
+          next: (res) => {
+            localStorage.setItem('token', res.token);
+          },
+          error: (err) => {
+            this.passwordError = err;
+          },
+        });
+    }
+  }
+}
