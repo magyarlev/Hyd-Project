5b7b3ff2355b1c2c61f0dc498fe721076e6309b6
MagyarL <magyar.levi@gmail.com>
2025-03-31 10:59:57 +0200

    updtate styling of login/register, update paths, add routing

--- hyd/src/app/login-register/register/register.component.ts
+++ hyd/src/app/login-register/register/register.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,14 +17,24 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
+    RouterLink,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  registerForm?: any;
+  fb = inject(FormBuilder);
+  registerForm: FormGroup;
   passwordError?: string;
+
   onSubmit() {
     throw new Error('Method not implemented.');
+  }
+
+  constructor() {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 }
