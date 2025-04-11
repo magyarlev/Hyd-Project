5aaff2b352dc55fec5c48757da45c88fa23c3baf
MagyarL <magyar.levi@gmail.com>
2025-04-11 13:29:53 +0200

    add auth guard to routes

--- hyd/src/app/app.routes.ts
+++ hyd/src/app/app.routes.ts
@@ -5,20 +5,24 @@ import { WriteAStoryComponent } from './write-a-story/write-a-story.component';
 import { LoginComponent } from './login-register/login/login.component';
 import { RegisterComponent } from './login-register/register/register.component';
 import { AdminComponent } from './admin/admin.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'write-a-story',
     component: WriteAStoryComponent,
+    canActivate: [authGuard],
   },
 
   {
     path: 'view-a-story',
     component: ViewAStoryComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'auth',
