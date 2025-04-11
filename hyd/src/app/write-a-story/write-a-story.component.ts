6d48b6fee6a9d5841cb2fe273dd23067ed766162
MagyarL <magyar.levi@gmail.com>
2025-04-11 13:30:26 +0200

    add snackbar, add query param

--- hyd/src/app/write-a-story/write-a-story.component.ts
+++ hyd/src/app/write-a-story/write-a-story.component.ts
@@ -1,12 +1,13 @@
 import { Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatInputModule } from '@angular/material/input';
-import { StoryService } from '../story.service';
-import { Story, StoryPOST } from '../types';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { StoryService } from '../story.service';
+import { StoryPOST } from '../types';
 
 @Component({
   selector: 'app-write-a-story',
@@ -21,6 +22,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   styleUrl: './write-a-story.component.scss',
 })
 export class WriteAStoryComponent {
+  #snackBar = inject(MatSnackBar);
+
+  #openSnackBar(message: string, action: string) {
+    this.#snackBar.open(message, action, { duration: 5000 });
+  }
+
   story: StoryPOST = {
     type: '',
     content: '',
@@ -35,7 +42,9 @@ export class WriteAStoryComponent {
         .addStory(this.story)
         .pipe(takeUntilDestroyed(this.destroyRef))
         .subscribe({
-          next: () => {},
+          next: () => {
+            this.#openSnackBar('Story added', 'OK!');
+          },
         });
     }
   }
