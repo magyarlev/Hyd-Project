ad63c9a72fd8eee6ee9f27d23d3e48f86f29ba75
MagyarL <magyar.levi@gmail.com>
2025-05-18 11:40:45 +0200

    add testing for view a story

--- hyd/src/app/view-a-story/view-a-story.component.spec.ts
+++ hyd/src/app/view-a-story/view-a-story.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViewAStoryComponent } from './view-a-story.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 describe('ViewAStoryComponent', () => {
   let component: ViewAStoryComponent;
@@ -21,5 +22,17 @@ describe('ViewAStoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should render a div with class "btn-wrapper" with 2 buttons inside it`, () => {
+    spyOn(component, 'getRandomStory');
+    const buttonWrapper = fixture.debugElement.query(By.css('.btn-wrapper'));
+    const buttons = buttonWrapper.queryAll(By.css('button'));
+    const getGoodStoryButton = buttons[0];
+    const getBadStoryButton = buttons[1];
+    getGoodStoryButton.triggerEventHandler('click');
+    expect(component.getRandomStory).toHaveBeenCalledWith('good');
+    getBadStoryButton.triggerEventHandler('click');
+    expect(component.getRandomStory).toHaveBeenCalledWith('bad');
   });
 });
